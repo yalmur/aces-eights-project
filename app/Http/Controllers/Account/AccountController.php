6affd3e6515c4b2e00f305aa57a954d512a5f346
murat yalcin (murley) <yalmur@gmail.com>
2026-06-03 23:05:50 +0100

    feat: customer account shows real order history

--- app/Http/Controllers/Account/AccountController.php
+++ app/Http/Controllers/Account/AccountController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Account;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class AccountController extends Controller
 {
     public function index(): View
     {
-        return view('account.index', ['title' => 'My Account']);
+        $user   = Auth::user();
+        $orders = $user->orders()->with('items')->paginate(10);
+
+        return view('account.index', [
+            'title'  => 'My Account',
+            'user'   => $user,
+            'orders' => $orders,
+        ]);
     }
 }
