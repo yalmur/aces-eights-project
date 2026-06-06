34335330a61b800ea2a90aa50ce2c53673099cc6
murat yalcin (murley) <yalmur@gmail.com>
2026-06-06 02:40:56 +0100

    feat: add manifest configuration and tinker input script for project tracking

---
+++ tinker_input.php
@@ -0,0 +1,16 @@
+﻿$maxId = App\Models\Order::max("id") ?? 0;
+$order = App\Models\Order::create([
+  "user_id" => null,
+  "customer_name" => "Sound Test",
+  "customer_email" => "soundtest@test.com",
+  "customer_phone" => "07700000000",
+  "type" => "collection",
+  "status" => "accepted",
+  "subtotal" => 1000,
+  "delivery_fee" => 0,
+  "discount_amount" => 0,
+  "total" => 1000,
+  "payment_method" => "card",
+  "stripe_session_id" => "test_sound_" . time(),
+]);
+echo "Created order #" . $order->id . " (prev max: " . $maxId . ")";
